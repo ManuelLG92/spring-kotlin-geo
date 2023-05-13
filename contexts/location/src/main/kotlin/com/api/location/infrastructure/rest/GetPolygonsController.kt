b36ffb6b3e582b22dd631882f1e82a3c7ffd04cc
ManuelLG92 <manuelleongodinho@gmail.com>
2023-05-14 01:23:25 +0200

    fix: variable name

--- contexts/location/src/main/kotlin/com/api/location/infrastructure/rest/GetPolygonsController.kt
+++ contexts/location/src/main/kotlin/com/api/location/infrastructure/rest/GetPolygonsController.kt
@@ -3,7 +3,6 @@ package com.api.location.infrastructure.rest
 
 import com.api.location.application.GetPolygonsHandler
 import com.api.location.domain.Polygon
-import com.api.location.application.dto.output.PolygonList
 import com.api.shared.infrastructure.dto.ListResponse
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
@@ -14,13 +13,14 @@ import org.springframework.web.bind.annotation.RestController
 class GetPolygonsController {
 
     @Autowired
-    private lateinit var handler: GetPolygonsHandler
+    private lateinit var getPolygonsHandler: GetPolygonsHandler
 
     @GetMapping("polygons")
     fun polygons(): ResponseEntity<ListResponse<Polygon>> {
-        val result = handler.execute()
-        val response = PolygonList(data = result)
+        val result = getPolygonsHandler.execute()
+        val response = ListResponse(data = result)
         return ResponseEntity.ok(response)
     }
+
 
 }

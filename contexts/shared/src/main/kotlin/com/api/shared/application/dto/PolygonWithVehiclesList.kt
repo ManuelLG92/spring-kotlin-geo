de2b47bb1d370b656b95d9d03e2c2922ac1e6227
ManuelLG92 <manuelleongodinho@gmail.com>
2023-05-14 01:31:46 +0200

    feat: - Replace interfaces for generic classes.

--- contexts/shared/src/main/kotlin/com/api/shared/application/dto/PolygonWithVehiclesList.kt
+++ contexts/shared/src/main/kotlin/com/api/shared/application/dto/PolygonWithVehiclesList.kt
@@ -1,7 +1,5 @@
 package com.api.shared.application.dto
 
-import com.api.shared.infrastructure.dto.ListResponse
-
 data class PolygonWithVehicles(val id: String, val vehicles: List<String>)
 
-data class PolygonWithVehiclesList(override val data: List<PolygonWithVehicles>) : ListResponse<PolygonWithVehicles>
+data class PolygonWithVehiclesList(val data: List<PolygonWithVehicles>)

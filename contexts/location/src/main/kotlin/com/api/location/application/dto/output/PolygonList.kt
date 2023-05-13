1fc73387e5c814418cf4c4fbaa3927cd4c1dd8a1
ManuelLG92 <manuelleongodinho@gmail.com>
2023-05-14 01:31:07 +0200

    feat: - Remove unsued file

--- contexts/location/src/main/kotlin/com/api/location/application/dto/output/PolygonList.kt
+++
@@ -1,6 +0,0 @@
-package com.api.location.application.dto.output
-
-import com.api.location.domain.Polygon
-import com.api.shared.infrastructure.dto.ListResponse
-
-data class PolygonList(override val data: List<Polygon>) : ListResponse<Polygon>

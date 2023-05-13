e57ebe5266bbbab33d6b4e16e171809c16d1e59e
ManuelLG92 <manuelleongodinho@gmail.com>
2023-05-14 01:32:12 +0200

    feat: - Add new interface for use case handler with parameter

--- contexts/shared/src/main/kotlin/com/api/shared/application/helpers/UseCasesHandler.kt
+++ contexts/shared/src/main/kotlin/com/api/shared/application/helpers/UseCasesHandler.kt
@@ -1,6 +1,10 @@
 package com.api.shared.application.helpers
 
 
+interface UseCaseHandler<in I, out O> {
+    fun execute(data: I): O
+}
+
 interface UseCaseHandlerWithoutParameters<out O> {
     fun execute(): O
 }

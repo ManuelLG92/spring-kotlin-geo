5304f1118d6e191c476423a0f21648ce70136cdc
ManuelLG92 <manuelleongodinho@gmail.com>
2023-05-15 19:45:36 +0200

    feat: - Unit testing

---
+++ contexts/location/src/test/kotlin/com/api/location/infrastructure/mappers/PolygonsArrayTest.kt
@@ -0,0 +1,37 @@
+package com.api.location.infrastructure.mappers
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertTrue
+
+class PolygonsArrayTest {
+
+    @Test
+    fun testShouldTest() {
+        val jsonPath = "src/main/resources/polygons1.json"
+        val polygonsArray = PolygonsArray()
+        val result = polygonsArray.loadFromJson(jsonPath)
+        assertTrue { result.size == 1 }
+        val firstItem = result.first()
+        assertTrue { firstItem.id == "58a58bf685979b5415f3a39a" }
+        assertTrue { firstItem.name == "0" }
+        assertTrue { firstItem.legacyId == "18_92" }
+        val expectedCoordinates: List<List<List<Float>>> = listOf(
+            listOf(
+                listOf(
+                    9.1372480.toFloat(), 48.790411.toFloat()
+                ),
+                listOf(
+                    9.137248.toFloat(), 48.790263.toFloat()
+                ),
+                listOf(
+                    9.13695.toFloat(), 48.790263.toFloat()
+                ),
+                listOf(
+                    9.137248.toFloat(), 48.790411.toFloat()
+                )
+            )
+        )
+        assertTrue { firstItem.geometry.coordinates == expectedCoordinates }
+
+    }
+}
--- contexts/location/src/test/kotlin/com/api/location/infrastructure/persistence/LocationRepositoryImplementationTest.kt
+++
@@ -1,4 +0,0 @@
-package com.api.location.infrastructure.persistence
-
-class LocationRepositoryImplementationTest {
-}
